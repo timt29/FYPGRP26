a71ae14032c979354e953cdff4f64f9e071ebbc8
timt29 <timothytoh1129@gmail.com>
2025-07-16 18:18:42 +0800

    Draft Website

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import webbrowser
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("Mainpage.html")  # This loads your HTML file
+
+@app.route("/members")
+def members():
+    return render_template("Members.html")  # This loads your HTML file
+
+@app.route("/ProjectDoc")
+def ProjectDoc():
+    return render_template("ProjectDoc.html")  # This loads your HTML file
+
+
+
+def open_browser():
+    webbrowser.open_new("http://127.0.0.1:5000/")
+
+if __name__ == "__main__":
+    threading.Timer(1.0, open_browser).start()
+    app.run(debug=True)
