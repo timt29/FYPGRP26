7a0217533b3593e9e3dfcec7b3c8a61218cebb2e
timt29 <timothytoh1129@gmail.com>
2025-09-26 01:28:36 +0800

    test 5

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def get_db_connection():
         password=os.environ["DB_PASSWORD"],
         database=os.environ["DB_NAME"]
     )
+print("Connected to MySQL")
 # ---------- Auth wrapper ---------- # (YY)
 def login_required(user_type):
     """
