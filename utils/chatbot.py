066a8aad55469a988880458b705649c8df5d53c4
strawnanaberry <ziting097@gmail.com>
2025-10-22 18:15:56 +0800

    Update chatbot.py

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -3,11 +3,9 @@ from flask import jsonify;
 FAQ_Responses = {
     "hello": "Hi there! How can I help you today?",
     "search": "Enter a keyword into the search bar at the top of the webpage and click on the search button.",
-
-    "how to create article": "To create an article, you must have a registered account. Once you logged in, click on the 'Create Your Story' on the right column.",
-    "how to share article": "To share an article, visit any article page and scroll to the end of the article, look out for the share button and click on it to share the article",
-    "how to register": "For all new users, kindly click on the orange 'register' button at the top right corner of the navigation bar.",
-    "how to search for article": "Enter a keyword into the search bar at the top of the webpage and click on the search button."
+    "create": "To create an article, you must have a registered account. Once you logged in, click on the 'Create Your Story' on the right column.",
+    "share": "To share an article, visit any article page and scroll to the end of the article, look out for the share button and click on it to share the article",
+    "register": "For all new users, kindly click on the orange 'register' button at the top right corner of the navigation bar."
 }
 
 def getChatbotResponse (user_input):
