a5ec86efb4b897f242e2ac1e7a2ae6decb9f07ed
timt29 <timothytoh1129@gmail.com>
2025-10-24 19:08:06 +0800

    added visible column in comment
    visible` tinyint NOT NULL DEFAULT '1',

--- database/nrs_comments.sql
+++ database/nrs_comments.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: nrs
 -- ------------------------------------------------------
--- Server version	8.0.43
+-- Server version	8.0.42
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,6 +32,7 @@ CREATE TABLE `comments` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `is_reply` tinyint(1) NOT NULL DEFAULT '0',
   `reply_to_comment_id` int DEFAULT NULL,
+  `visible` tinyint NOT NULL DEFAULT '1',
   PRIMARY KEY (`commentID`),
   KEY `idx_comments_articleID` (`articleID`),
   KEY `idx_comments_userID` (`userID`),
@@ -39,7 +40,7 @@ CREATE TABLE `comments` (
   CONSTRAINT `fk_comments_article` FOREIGN KEY (`articleID`) REFERENCES `articles` (`articleID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_comments_parent` FOREIGN KEY (`reply_to_comment_id`) REFERENCES `comments` (`commentID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_comments_user` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +49,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (1,1,1,'Seed comment #1 on article 1 by user 1',0,1,'2025-10-03 02:28:14',0,NULL),(2,2,2,'Seed comment #2 on article 2 by user 2',1,0,'2025-10-03 02:28:14',0,NULL),(3,3,3,'Seed comment #3 on article 3 by user 3',1,0,'2025-10-03 02:28:14',0,NULL),(4,4,4,'Seed comment #4 on article 4 by user 4',0,1,'2025-10-03 02:28:14',0,NULL),(5,5,5,'Seed comment #5 on article 5 by user 5',1,0,'2025-10-03 02:28:14',0,NULL),(6,1,1,'[SEED100-T001] Seed comment 1 on article 1 by user 1',0,0,'2025-10-03 02:31:22',0,NULL),(7,2,2,'[SEED100-T002] Seed comment 2 on article 2 by user 2',0,0,'2025-10-03 02:31:22',0,NULL),(8,3,3,'[SEED100-T003] Seed comment 3 on article 3 by user 3',0,0,'2025-10-03 02:31:22',0,NULL),(9,4,4,'[SEED100-T004] Seed comment 4 on article 4 by user 4',0,0,'2025-10-03 02:31:22',0,NULL),(10,5,10,'asda',0,0,'2025-10-05 11:22:00',0,NULL);
+INSERT INTO `comments` VALUES (1,1,1,'Seed comment #1 on article 1 by user 1',0,1,'2025-10-03 02:28:14',0,NULL,1),(2,2,2,'Seed comment #2 on article 2 by user 2',1,0,'2025-10-03 02:28:14',0,NULL,1),(3,3,3,'Seed comment #3 on article 3 by user 3',1,0,'2025-10-03 02:28:14',0,NULL,1),(4,4,4,'Seed comment #4 on article 4 by user 4',0,1,'2025-10-03 02:28:14',0,NULL,1),(5,5,5,'Seed comment #5 on article 5 by user 5',1,0,'2025-10-03 02:28:14',0,NULL,1),(6,1,1,'[SEED100-T001] Seed comment 1 on article 1 by user 1',0,0,'2025-10-03 02:31:22',0,NULL,1),(7,2,2,'[SEED100-T002] Seed comment 2 on article 2 by user 2',0,0,'2025-10-03 02:31:22',0,NULL,1),(8,3,3,'[SEED100-T003] Seed comment 3 on article 3 by user 3',0,0,'2025-10-03 02:31:22',0,NULL,1),(9,4,4,'[SEED100-T004] Seed comment 4 on article 4 by user 4',0,0,'2025-10-03 02:31:22',0,NULL,1),(10,5,10,'asda',0,0,'2025-10-05 11:22:00',0,NULL,1),(11,5,10,'testing',0,0,'2025-10-24 10:21:23',0,NULL,1),(12,5,10,'what the hell happened',0,0,'2025-10-24 10:21:38',0,NULL,1),(13,5,3,'lmao',0,0,'2025-10-24 10:22:08',0,NULL,1),(14,5,3,'lmao',0,0,'2025-10-24 10:22:08',0,NULL,1),(15,5,10,'@Sam hi sam this comment is damn funny',1,0,'2025-10-24 10:57:23',1,13,1),(16,5,10,'@random replying to myself is a feature too',0,0,'2025-10-24 10:57:59',1,12,1),(17,5,3,'@random thank you for your comment',0,0,'2025-10-24 11:01:03',1,13,1),(18,5,3,'@random testing nested comment',0,0,'2025-10-24 11:01:54',1,12,1);
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -61,4 +62,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-16 21:51:07
+-- Dump completed on 2025-10-24 19:07:04
