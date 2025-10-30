4ceb673b6c5ebe43aba553d5f1633fe6d1b0e890
timt29 <timothytoh1129@gmail.com>
2025-10-31 01:07:49 +0800

    testing supabase and reduce size of transformers
    transformers  huge module laaaaa

--- utils/content_analyzer.py
+++ utils/content_analyzer.py
@@ -5,24 +5,36 @@
 
 from transformers import pipeline
 
-# load pre-trained models
-from transformers import pipeline
+# Lazy-load models
+_sentiment_analyzer = None
+_toxicity_detector = None
 
-sentiment_analyzer = pipeline(
-    "sentiment-analysis", 
-    model="distilbert/distilbert-base-uncased-finetuned-sst-2-english"
-)
-toxicity_detector = pipeline(
-    "text-classification", 
-    model="unitary/toxic-bert"
-)
+def get_sentiment_analyzer():
+    global _sentiment_analyzer
+    if _sentiment_analyzer is None:
+        _sentiment_analyzer = pipeline(
+            "sentiment-analysis",
+            model="distilbert/distilbert-base-uncased-finetuned-sst-2-english",
+            device=-1  # CPU only
+        )
+    return _sentiment_analyzer
+
+def get_toxicity_detector():
+    global _toxicity_detector
+    if _toxicity_detector is None:
+        _toxicity_detector = pipeline(
+            "text-classification",
+            model="unitary/toxic-bert",
+            device=-1  # CPU only
+        )
+    return _toxicity_detector
 
 
 def analyze_content(article_text):
     text = article_text[:512]
 
-    sentiment = sentiment_analyzer(text)[0]
-    toxicity = toxicity_detector(text)[0]
+    sentiment = get_sentiment_analyzer()(text)[0]
+    toxicity = get_toxicity_detector()(text)[0]
 
     sentiment_label = sentiment['label']
     toxicity_label = toxicity['label']
